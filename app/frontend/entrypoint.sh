7389cca546eed091a67deda0f4660585bc661027
Amr Omran <amr.omran@blueworld.ai>
2025-05-17 21:16:16 +0400

    Update Nginx configurations for rootless operation

--- app/frontend/entrypoint.sh
+++ app/frontend/entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 
 : "${BACKEND_URL:=http://backend:3000}"
 
-# Substitute env var in server.conf.template to default.conf
-envsubst '$BACKEND_URL' < /etc/nginx/server.conf.template > /etc/nginx/conf.d/default.conf
+# Substitute env var in backend.conf.template to default.conf
+envsubst '$BACKEND_URL' < /tmp/backend.conf.template > /etc/nginx/conf.d/default.conf
+# Remove the template file to avoid confusion
+rm -rf /tmp/backend.conf.template
 
 exec nginx -g 'daemon off;'
